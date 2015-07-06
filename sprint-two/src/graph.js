e0ad06d60c00fdb9e838f0be19c2fcafee775a1e
Eric Fischer <ericf712@gmail.com>
2015-07-06 03:40:31 -0700

    Update graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,50 @@
 
 
 var Graph = function(){
+  this._nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+  if (node) {
+    this._nodes[node] = this._nodes[node] || { edges: [] };
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return !(this._nodes[node] === undefined);
 };
 
 Graph.prototype.removeNode = function(node){
+  if (this.contains(node)) {
+    for (var targetNode in this._nodes[node].edges) {
+      this.removeEdge(node, targetNode);
+    }
+    delete this._nodes[node];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return this._nodes[fromNode].edges.indexOf(toNode) !== -1;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if (fromNode === undefined || toNode === undefined) return null;
+  if (this._nodes[fromNode].edges.indexOf(toNode) === -1) { this._nodes[fromNode].edges.push(toNode) };
+  if (this._nodes[toNode].edges.indexOf(fromNode) === -1) { this._nodes[toNode].edges.push(fromNode) };
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var spliceIndex = this._nodes[fromNode].edges.indexOf(toNode);
+  if (spliceIndex !== -1) { this._nodes[fromNode].edges.splice(spliceIndex, 1) };
+
+  spliceIndex = this._nodes[toNode].edges.indexOf(fromNode);
+  if (spliceIndex !== -1) { this._nodes[toNode].edges.splice(spliceIndex, 1) };
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var node in this._nodes) {
+    cb(node);
+  }
 };
 
 /*
