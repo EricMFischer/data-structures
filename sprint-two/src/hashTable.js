0aedb05488c5d04fa0c4868b2dfefa2a9c563349
Eric Fischer <ericf712@gmail.com>
2015-07-12 21:41:12 -0700

    Optimizes hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,48 +5,78 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  // 1. Get index (address) for our key (k)
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
-  if (this._storage[i] === undefined) {
-    this._storage[i] = [];
+  // Get index (address) for our key k
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var bucket = this._storage.get(index); // Retrieve bucket at index
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
   }
 
-  // 2. Once we have address, insert into our table at 'i' (address!)
-  this._storage[i].push([k,v]);
+  // Iterate over bucket to check for same key--if found, overwrite
+  for (var i=0; i<bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {tuple[1] = v; return;}
+  }
+
+  bucket.push([k,v]); // Store tuple in bucket (which can have mult. tuples)
+  this._size++;
+  if (this._size > 0.75 * this._limit) {
+    this.resize(this._limit * 2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
-  // 1. Get index (address) for our key (k)
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  // Go to storage[i]
+  var index = getIndexBelowMaxForKey(k, this._limit);
   
-  // Count number of elements at storage[i] (Tooples?)
-  for (var x=0; x<this._storage[i].length; x++) {
-    if (this._storage[i][x][0] === k) {
-      return this._storage[i][x][1];
-    }
+  var bucket = this._storage.get(index);
+  if (!bucket) {return null;}
+
+  for (var i=0; i<bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {return tuple[1];}
   }
-      // Iterate through each toople
-        // Find where [i][x][0] === k
-        // Return v
+
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
 
-  for (var x=0; x<this._storage[i].length; x++) {
-    if (this._storage[i][x][0] === k) {
-      this._storage[i][x][1] = null;
+  var bucket = this._storage.get(index);
+  if (!bucket) {return null;}
+
+  for (var i=0; i<bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      this._size--;
+      if (this._size < 0.25 * this._limit) {
+        this.resize(this._limit / 2);
+      }
+      return tuple[1];
     }
   }
+  
+  return null;
+};
+
+HashTable.prototype.resize = function(newSize){
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(newSize);
+  this._limit = newSize;
+  this._size = 0;
+
+  oldStorage.each(function(bucket){
+    if (!bucket) {return;}
+    for (var i=0; i<bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-/*
- var myTable = new HashTable();
- myTable.insert('val1','val1');
- myTable.insert('val2','val2');
