cd68abe89dab2783e30023f4bd1a1e48f33702b5
Eric Fischer <ericf712@gmail.com>
2015-07-16 23:47:44 -0700

    Updates functional-shared classes

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,30 +1,27 @@
 var Queue = function(){
-  var someInst = {};
-  someInst.storage = {};
-  someInst.start = -1;
-  someInst.end = -1;
-  extend(someInst, queueMethods);
-  
-  return someInst;
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.start = -1; // start and end --> called diff things, but ultimately both get incremented
+  someInstance.end = -1;   // as a result of enqueuing and dequeuing, respectively
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
-queueMethods.enqueue = function(value) {  // add to back
-	this.end++;
-	this.storage[this.end] = value;
-};
+queueMethods.enqueue = function(value){ // add to back
+    this.end++;
+    this.storage[this.end] = value;
+  };
 
-queueMethods.dequeue = function() {  // remove from front
-	this.start++;
-	var result = this.storage[this.start];
+queueMethods.dequeue = function(){ // remove from front
+  this.start++;
+  var result = this.storage[this.start];
   delete this.storage[this.start];
   return result;
 };
 
-queueMethods.size = function() {
-  if (this.end - this.start <= 0) {
-    return 0;
-  }
+queueMethods.size = function(){
+  if (this.end - this.start < 0) {return 0;}
   return this.end - this.start;
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,36 +1,90 @@
 var Stack = function() {
-  var someInst = {};
-  someInst.storage = {};
-  someInst._size = 0;
-  _.extend(someInst, stackMethods);
-
-  return someInst;
+  var someInstance = {};
+  _.extend(someInstance, stackMethods);
+  someInstance._size = 0;
+  someInstance._storage = {};
+  return someInstance;
 };
 
 var stackMethods = {};
 
 stackMethods.push = function(value) {
-	this.storage[this._size] = value;
+  this._storage[this._size] = value;
   this._size++;
-};
+}
 
 stackMethods.pop = function() {
-	this._size--;
-	var result = this.storage[this._size];
-  delete this.storage[this._size];
+  this._size--;
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
   return result;
-};
+}
 
 stackMethods.size = function() {
-  if (this._size <= 0) {
-    return 0;
-  }
+  if (this._size < 0) {return 0;}
   return this._size;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var Stack = function(){
+  var someInstance = {}; // functional: instantiates an object for us
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var length = 0;
+
+  someInstance.push = function(value){
+    storage[length] = value;
+    length++; // needs to after, e.g. if length is 3, you want to push in at position [3], not [4]
+  };
+
+  someInstance.pop = function(){
+    length--; // needs to go before, e.g. length may be 3, but that means you want to remove at position [2]
+    var result = storage[length];
+    delete storage[length];
+    return result;
+  };
+
+  someInstance.size = function(){
+    if (length < 0) {return 0;}
+    return length;
+  };
+
+  return someInstance;
 };
 
-// Example of extend
-var extend = function(to, from) {
-	for (var key in from) {
-		to[key] = from[key];
-	}
-}
+*/
