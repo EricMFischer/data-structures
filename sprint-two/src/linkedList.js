6132fb360ad899179d30c3c57fa60c65aa46b7f9
Eric Fischer <ericf712@gmail.com>
2015-07-16 23:51:12 -0700

    Optimizes linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,37 +5,37 @@ var LinkedList = function(){
 
   list.addToTail = function(value){ // constant time
     var newTail = Node(value);
-    if (!list.head) {list.head = newTail;}
-    if (list.tail) {list.tail.next = newTail;}
-    list.tail = newTail;  // update list.tail
+    if (list.head === null) {list.head = newTail;}
+    else {list.tail.next = newTail;}
+    list.tail = newTail; // update list.tail
   };
 
-  list.removeHead = function(){ // consant time
+  list.removeHead = function(){ // constant time
     if (list.head === null) {return null;}
     var currentHead = list.head;
-    list.head = list.head.next;  // update list.head
+    list.head = list.head.next; // update list.head
     return currentHead.value;
   };
 
   list.contains = function(target){ // linear time
     var node = list.head;
     while (node) {
-      if (node.value === target) {return true;}
+      if (node.value === target) {
+        return true;
+      }
       node = node.next;
     }
     return false;
-  };
+  }
 
   return list;
 };
 
 var Node = function(value){
   var node = {};
+
   node.value = value;
   node.next = null;
+
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
